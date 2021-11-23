fde06ef378e3f124a0f90c1e4f32545e913c7f2d
Remedy Mix Jumanji <83578946+Remedy-Mix1010@users.noreply.github.com>
2021-11-23 20:46:08 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var images = ["fam.gif","me.jpg", "bro.svg" , "dad", "mom.jpg", "pet.jfif"];
+var names = ["Our Simple SquadFAM","me.", "my bro.", "daddy cool.", "mummy awesome.", "my pretend dog."];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
